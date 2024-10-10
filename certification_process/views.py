5cd5f736f93639d6e5200d2a3b610ffc2a18de74
Dennis Schenkel <dennisschenkel@googlemail.com>
2024-10-10 21:59:52 +0200

    Update requirements

--- certification_process/views.py
+++ certification_process/views.py
@@ -8,8 +8,8 @@ from .serializers import (
     CategorySerializer,
     CriterionSerializer,
     ProjectSerializer,
-    ProjectCriterionSerializer
-    )
+    ProjectCriterionSerializer,
+)
 import logging
 
 logger = logging.getLogger(__name__)
@@ -26,7 +26,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
 
     def get_queryset(self):
-        section_id = self.kwargs.get('section_pk')
+        section_id = self.kwargs.get("section_pk")
         logger.debug(f"Filtering categories for section_id: {section_id}")
         return Category.objects.filter(section_id=section_id)
 
@@ -37,7 +37,7 @@ class CriterionViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         # `category_pk` wird durch den verschachtelten Router bereitgestellt
-        category_id = self.kwargs.get('category_pk')
+        category_id = self.kwargs.get("category_pk")
         logger.debug(f"Filtering criteria for category_id: {category_id}")
         return Criterion.objects.filter(category_id=category_id)
 
@@ -49,14 +49,14 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
     @action(
         detail=True,
-        methods=['get', 'post'],
-        url_path='criteria/(?P<criterion_id>[^/.]+)/result',
-        url_name='criterion-result'
+        methods=["get", "post"],
+        url_path="criteria/(?P<criterion_id>[^/.]+)/result",
+        url_name="criterion-result",
     )
     def criterion_result(self, request, pk=None, criterion_id=None):
         project = self.get_object()
 
-        if request.method == 'GET':
+        if request.method == "GET":
             # Abrufen des spezifischen ProjectCriterion-Objekts
             project_criterion = ProjectCriterion.objects.filter(
                 project=project, criterion_id=criterion_id
@@ -65,42 +65,32 @@ class ProjectViewSet(viewsets.ModelViewSet):
             if not project_criterion:
                 return Response(
                     {"detail": "ProjectCriterion not found."},
-                    status=status.HTTP_404_NOT_FOUND
+                    status=status.HTTP_404_NOT_FOUND,
                 )
 
             serializer = ProjectCriterionSerializer(project_criterion)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        elif request.method == 'POST':
+        elif request.method == "POST":
             serializer = ProjectCriterionSerializer(data=request.data)
             if serializer.is_valid():
-                project_criterion, created = ProjectCriterion.objects.update_or_create(
+                project_criterion,
+                created = ProjectCriterion.objects.update_or_create(
                     project=project,
                     criterion_id=criterion_id,
                     defaults={
-                        'status': serializer.validated_data.get(
-                            'status', 'in_scope'
-                            ),
-                        'note': serializer.validated_data.get('note', '')
-                    }
+                        "status": serializer.validated_data.get("status", "in_scope"),
+                        "note": serializer.validated_data.get("note", ""),
+                    },
                 )
-                response_serializer = ProjectCriterionSerializer(
-                    project_criterion
-                    )
+                response_serializer = ProjectCriterionSerializer(project_criterion)
                 if created:
                     return Response(
-                        response_serializer.data,
-                        status=status.HTTP_201_CREATED
-                        )
+                        response_serializer.data, status=status.HTTP_201_CREATED
+                    )
                 else:
-                    return Response(
-                        response_serializer.data,
-                        status=status.HTTP_200_OK
-                        )
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-                )
+                    return Response(response_serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ProjectCriterionViewSet(viewsets.ModelViewSet):
@@ -110,7 +100,7 @@ class ProjectCriterionViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = ProjectCriterion.objects.all()
-        project_id = self.request.query_params.get('project_id')
+        project_id = self.request.query_params.get("project_id")
         if project_id is not None:
             queryset = queryset.filter(project__id=project_id)
         return queryset
